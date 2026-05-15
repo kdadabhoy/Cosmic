a998fa89338baaf3cd5c1b5023cd912becdaedd2
kdadabhoy <kdadabhoy28@gmail.com>
2026-05-14 17:56:09 -0700

    Shader Documentation

--- Cosmic/src/graphics/Shader.h
+++ Cosmic/src/graphics/Shader.h
@@ -66,22 +66,22 @@ namespace Cosmic
 		////////////////////////////////
 		// Pipeline State
 		///////////////////////////////
-		virtual void Bind() const = 0;
-		virtual void Unbind() const = 0;
+		virtual void			Bind() const = 0;
+		virtual void			Unbind() const = 0;
 
 		////////////////////////////////
 		// Uniform Upload (CPU to GPU)
 		///////////////////////////////
-		virtual void SetInt(const std::string& name, int value) = 0;
-		virtual void SetIntArray(const std::string& name, int* values, uint32_t count) = 0;
-		virtual void SetFloat3(const std::string& name, const glm::vec3& value) = 0;
-		virtual void SetFloat4(const std::string& name, const glm::vec4& value) = 0;
-		virtual void SetMat3(const std::string& name, const glm::mat3& value) = 0;
-		virtual void SetMat4(const std::string& name, const glm::mat4& value) = 0;
+		virtual void			SetInt(const std::string& name, int value)							= 0;
+		virtual void			SetIntArray(const std::string& name, int* values, uint32_t count)	= 0;
+		virtual void			SetFloat3(const std::string& name, const glm::vec3& value)			= 0;
+		virtual void			SetFloat4(const std::string& name, const glm::vec4& value)			= 0;
+		virtual void			SetMat3(const std::string& name, const glm::mat3& value)			= 0;
+		virtual void			SetMat4(const std::string& name, const glm::mat4& value)			= 0;
 
 		////////////////////////////////
 		// Factory Pattern
 		///////////////////////////////
-		static Ref<Shader> Create(const std::string& filepath);
+		static Ref<Shader>		Create(const std::string& filepath);
 	};
 }
